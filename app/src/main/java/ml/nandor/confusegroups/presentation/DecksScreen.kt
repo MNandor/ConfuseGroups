0d2108fc965ff3610126e2cbfa1aee2ccd3aa279
MNandor <mnandor@users.noreply.github.com>
2023-09-24 17:08:24 +0300

    add edit button to deck screen

--- app/src/main/java/ml/nandor/confusegroups/presentation/DecksScreen.kt
+++ app/src/main/java/ml/nandor/confusegroups/presentation/DecksScreen.kt
@@ -2,13 +2,19 @@ package ml.nandor.confusegroups.presentation
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -33,21 +39,35 @@ private fun DeckItem(text: String, viewModel: MainViewModel) {
             .padding(16.dp)
             .fillMaxWidth()
             .height(128.dp)
-            .combinedClickable (
+            .combinedClickable(
                 onClick = {
                     viewModel.selectDeck(text)
                 }
             )
     ) {
-        Text(
-            text = text,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-                .wrapContentSize(),
-            textAlign = TextAlign.Center,
-            fontSize = 32.sp
-        )
+        Column(){
+            Text(
+                text = text,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+                    .wrapContentSize()
+                    .height(64.dp)
+                ,
+                textAlign = TextAlign.Center,
+                fontSize = 32.sp
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ){
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Edit, contentDescription = "Favorite")
+                }
+            }
+        }
+
 
     }
 }
