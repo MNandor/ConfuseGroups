25b1fc045508eb92a1deb7e3ac06ecfa07fc44ff
MNandor <mnandor@users.noreply.github.com>
2023-09-27 22:19:26 +0300

    display correlations visually

--- app/src/main/java/ml/nandor/confusegroups/presentation/ComparisonScreen.kt
+++ app/src/main/java/ml/nandor/confusegroups/presentation/ComparisonScreen.kt
@@ -1,12 +1,21 @@
 package ml.nandor.confusegroups.presentation
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,6 +28,10 @@ fun ComparisonScreen(viewModel: MainViewModel){
 
     val deckName = viewModel.comparisonDeck.value!!
 
+    BackHandler(onBack = {
+        viewModel.setComparisonDeck(null)
+    })
+
     Column() {
         Text(
             text = deckName,
@@ -26,16 +39,71 @@ fun ComparisonScreen(viewModel: MainViewModel){
                 .fillMaxWidth()
                 .padding(top = 16.dp)
                 .wrapContentSize()
-                .height(64.dp)
             ,
             fontSize = 32.sp
         )
         
         LazyColumn {
             items(items = viewModel.correlations.value) {it ->
-                Text(it.toString())
+                OutlinedCard (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1.0f)
+                        .padding(16.dp)
+                ){
+
+                    Text(
+                        text = (it.correlation*100).toInt().toString()+"%",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp)
+                            .wrapContentSize()
+                        ,
+                        fontSize = 16.sp
+                    )
+
+                    Row(
+                        modifier = Modifier
+                            .weight(1.0f)
+                    ) {
+                        SimpleSmallCard(it.leftCard.question)
+                        SimpleSmallCard(it.rightCard.question)
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .weight(1.0f)
+                    ) {
+                        SimpleSmallCard(it.leftCard.answer)
+                        SimpleSmallCard(it.rightCard.answer)
+                    }
+                }
+
 
             }
         }
+    }
+}
+
+@Composable
+fun RowScope.SimpleSmallCard(text:String, ){
+    ElevatedCard(
+        modifier = Modifier
+            .weight(1.0f)
+            .padding(16.dp)
+            .fillMaxWidth()
+            .aspectRatio(1.0f)
+
+
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .wrapContentSize(),
+            textAlign = TextAlign.Center,
+            fontSize = 32.sp
+        )
     }
 }
