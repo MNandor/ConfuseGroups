e505a392f209d677abe1dcb8d77d8c4d54b9c504
MNandor <mnandor@users.noreply.github.com>
2023-10-01 23:17:05 +0300

    autoresize text experiment

--- app/src/main/java/ml/nandor/confusegroups/presentation/ComparisonPopup.kt
+++ app/src/main/java/ml/nandor/confusegroups/presentation/ComparisonPopup.kt
@@ -20,12 +20,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -34,16 +38,16 @@ fun ComparisonPopup(text: String, viewModel: MainViewModel) {
 
 
     Box( modifier = Modifier
-        .offset{IntOffset(offset.x.toInt(), offset.y.toInt())}
+        .offset { IntOffset(offset.x.toInt(), offset.y.toInt()) }
         .pointerInput(Unit) {
             detectDragGestures { change, dragAmount ->
-            change.consume()
-                offset = Offset(offset.x + dragAmount.x, offset.y+dragAmount.y)
+                change.consume()
+                offset = Offset(offset.x + dragAmount.x, offset.y + dragAmount.y)
             }
         }
         .fillMaxWidth(0.3f)
         .aspectRatio(1.0f)
-        .combinedClickable (
+        .combinedClickable(
             onClick = {},
             onLongClick = {
                 viewModel.removeComparisonPopup(text)
@@ -55,7 +59,7 @@ fun ComparisonPopup(text: String, viewModel: MainViewModel) {
                 .fillMaxSize()
 
         ) {
-            Text(text)
+            AutoResizeText(modifier = Modifier.fillMaxSize(), text = text, fontSizeRange = FontSizeRange(min = 12.sp, max = 128.sp), maxLines = 1, textAlign = TextAlign.Center)
         }
     }
 
