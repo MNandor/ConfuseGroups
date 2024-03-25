9fdea009a49667e46bab76f4139a6ab297339681
MNandor <mnandor@users.noreply.github.com>
2024-03-25 16:36:23 +0200

    improve design of card view item

--- app/src/main/java/ml/nandor/confusegroups/presentation/cards/CardsScreen.kt
+++ app/src/main/java/ml/nandor/confusegroups/presentation/cards/CardsScreen.kt
@@ -1,12 +1,23 @@
 package ml.nandor.confusegroups.presentation.cards
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import ml.nandor.confusegroups.domain.model.AtomicNote
 import ml.nandor.confusegroups.presentation.common.CommonViewModel
 import timber.log.Timber
 
@@ -25,10 +36,43 @@ fun CardsScreen(commonViewModel: CommonViewModel){
             val cards = localViewModel.deckCards.value
             LazyColumn {
                 items(items = cards) { item ->
-                    Text("${item.question} - ${item.answer}")
+                    OneCard(item)
 
                 }
             }
         }
+    }
+}
+
+@Composable
+fun OneCard(card: AtomicNote){
+    ElevatedCard (
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+
+    ){
+
+        Column {
+            Text("[[${card.id}]]", fontSize = 12.sp,  modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                color = LocalContentColor.current.copy(alpha = 0.5f)
+            )
+            Row {
+                Text(card.question ?: "[[MISSING]]", fontSize = 16.sp,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(.5f),
+                )
+                Text(card.answer, fontSize = 16.sp,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(.5f)
+                )
+            }
+        }
+
     }
 }
