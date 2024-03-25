883e29e752c569d26c4c3923f463cd0ace04c565
MNandor <mnandor@users.noreply.github.com>
2024-03-25 18:56:09 +0200

    fix empty decks showing 1 card

--- app/src/main/java/ml/nandor/confusegroups/data/DataAccessObject.kt
+++ app/src/main/java/ml/nandor/confusegroups/data/DataAccessObject.kt
@@ -55,7 +55,7 @@ interface DataAccessObject {
     fun getNotesMatchingAnswer(answer:String):List<AtomicNote>
 
     // todo this returns 1 when it should return 0
-    @Query("SELECT name, count(*) FROM Deck LEFT JOIN AtomicNote ON deck.name = AtomicNote.deck GROUP BY deck.name")
+    @Query("SELECT name, count(*) FROM Deck INNER JOIN AtomicNote ON deck.name = AtomicNote.deck GROUP BY deck.name")
     fun getDeckSizes():List<DeckSize>
 
     @Query("UPDATE DECK SET displayName = :deckNewName WHERE name = :deckID")
