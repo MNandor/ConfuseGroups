d4958f588fb125b68a9a3ad7ada89f2ec67e66a1
MNandor <mnandor@users.noreply.github.com>
2024-04-25 03:40:48 +0300

    add a Jetpack Compose @Preview

--- app/src/main/java/ml/nandor/confusegroups/presentation/item/NoteInAList.kt
+++ app/src/main/java/ml/nandor/confusegroups/presentation/item/NoteInAList.kt
@@ -1,13 +1,16 @@
 package ml.nandor.confusegroups.presentation.item
 
+import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ml.nandor.confusegroups.domain.model.AtomicNote
@@ -19,12 +22,24 @@ fun NoteInAList(it: AtomicNote, callback: (AtomicNote) -> Unit = {}) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(4.dp)
-            .combinedClickable (
+            .combinedClickable(
                 onClick = {
                     callback(it)
                 }
             )
     ) {
         Text("${it.question} - ${it.answer}", fontSize = 24.sp, modifier = Modifier.padding(8.dp))
+    }
+}
+
+@Preview
+@Composable
+fun SampleNoteInAList(){
+    val note = AtomicNote("1A2B3C", "Woof", "Animals", "Dog", "Dogs bark")
+
+    Column(){
+
+        NoteInAList(it = note, {})
+        NoteInAList(it = note, {})
     }
 }
