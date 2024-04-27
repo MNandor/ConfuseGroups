3258edd8ec95cad1e3c9b76ee9f50406cd0d0818
MNandor <mnandor@users.noreply.github.com>
2024-04-27 18:53:28 +0300

    add button to export entire db

--- app/src/main/java/ml/nandor/confusegroups/presentation/xport/XportScreen.kt
+++ app/src/main/java/ml/nandor/confusegroups/presentation/xport/XportScreen.kt
@@ -1,7 +1,9 @@
 package ml.nandor.confusegroups.presentation.xport
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
@@ -11,23 +13,34 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import ml.nandor.confusegroups.presentation.common.CommonViewModel
+import ml.nandor.exportDatabaseFile.ExportDatabaseFile
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun XportScreen(commonViewModel: CommonViewModel){
 
     val localViewModel:XportViewModel = hiltViewModel()
+    val context = LocalContext.current
 
     BackHandler(onBack = {
         commonViewModel.deselectDeck()
     })
 
     Column {
-        Button(onClick = { localViewModel.loadExportString(commonViewModel.selectedDeck.value) }, modifier = Modifier.fillMaxWidth()) {
-            Text("Export")
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround){
+            Button(onClick = { localViewModel.loadExportString(commonViewModel.selectedDeck.value) }) {
+                Text("Export to Text")
+
+            }
+            Button(onClick = { ExportDatabaseFile.exportDBToStorage(context) }) {
+                Text("Export Entire DB")
+
+            }
 
         }
         TextField(value = localViewModel.exportString.value, onValueChange = {}, modifier = Modifier
